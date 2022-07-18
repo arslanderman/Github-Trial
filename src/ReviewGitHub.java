dca91e3dffe57845b63759e55cb5a727585e2097
Arslan Derman <arslandermanson@gmail.com>
2022-07-19 00:36:47 +0200

    New code added

--- src/ReviewGitHub.java
+++ src/ReviewGitHub.java
@@ -1,4 +1,9 @@
 public class ReviewGitHub {
 
+    public static void main(String[] args) {
+
+        System.out.println("hi everyone");
+        System.out.println(" i am learning github");
+    }
 
 }
