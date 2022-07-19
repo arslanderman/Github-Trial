818987844e58fe4f75408e0a809c0953189aeb2a
Arslan Derman <arslandermanson@gmail.com>
2022-07-19 16:37:51 +0200

    String manipulation

--- src/ReviewGitHub.java
+++ src/ReviewGitHub.java
@@ -4,6 +4,9 @@ public class ReviewGitHub {
 
         System.out.println("hi everyone");
         System.out.println(" i am learning github");
+
+        String str = "Hello World";
+        System.out.println(str.substring(3,8));
     }
 
 }
