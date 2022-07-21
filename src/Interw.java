ada236670247c40464ca0125d82d33b9a583666d
Arslan Derman <arslandermanson@gmail.com>
2022-07-21 19:04:31 +0200

    reverse string

---
+++ src/Interw.java
@@ -0,0 +1,18 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Interw {
+    public static void main(String[] args) {
+
+        String str = " Here is Java ";
+        String reverse = "";
+        for ( int i= str.length()-1;i>=0;i--){
+            reverse = reverse+ str.charAt(i);
+
+        }
+        System.out.println(reverse);
+
+
+
+    }
+}
